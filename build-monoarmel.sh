9c003d21940663c064bb6ac5e246ec1d43cbc81b
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2016-01-10 14:48:17 +0100

    Set Maintainer

--- build-monoarmel.sh
+++ build-monoarmel.sh
@@ -5,6 +5,7 @@ git clone https://github.com/mono/docker.git mono-docker
 cd mono-docker
 sed -i "s/FROM debian/FROM armel\/debian/" */Dockerfile
 sed -i "/.*deb http:\/\/download.mono-project.com\/repo\/debian [0-9]\+-security main.*/d" */Dockerfile
+sed -i "s/MAINTAINER .*/MAINTAINER <https:\/\/github.com\/StefanSchoof\/aspnetarmel>/" */Dockerfile
 for version in $(ls -1d */)
 do
 	if [[ ! " ${noBuild[@]} " =~ " ${version} " ]]; then
