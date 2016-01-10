d1d3aea2a9092ec03b76afb5665ff862b01a62c8
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2016-01-10 11:39:47 +0100

    Add build script for mono

---
+++ build-monoarmel.sh
@@ -0,0 +1,15 @@
+noBuild=("3.8.0/" "3.10.0/")
+
+rm -rf mono-docker
+git clone https://github.com/mono/docker.git mono-docker
+cd mono-docker
+sed -i "s/FROM debian/FROM armel\/debian/" */Dockerfile
+sed -i "/.*deb http:\/\/download.mono-project.com\/repo\/debian [0-9]\+-security main.*/d" */Dockerfile
+for version in $(ls -1d */)
+do
+	if [[ ! " ${noBuild[@]} " =~ " ${version} " ]]; then
+		docker build -t aspnetarmel/armel-mono:${version%/} $version
+		docker push aspnetarmel/armel-mono:${version%/}
+	fi
+done
+
