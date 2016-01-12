900cec20ef4e8ad52382f239c9482dc964a2706a
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2016-01-12 16:55:02 +0100

    move sed commands to variables

--- build-monoarmel.sh
+++ build-monoarmel.sh
@@ -3,14 +3,17 @@ GitFolder="mono-docker"
 GitUrl="https://github.com/mono/docker.git"
 FolderGlob="*/"
 ImageName="aspnetarmel/armel-mono"
+SedCommands='
+s/FROM debian/FROM armel\/debian/
+/.*deb http:\/\/download.mono-project.com\/repo\/debian [0-9]\+-security main.*/d
+s/MAINTAINER .*/MAINTAINER <https:\/\/github.com\/StefanSchoof\/aspnetarmel>/
+'
 
 rm -rf $GitFolder
 git clone $GitUrl $GitFolder
 docker pull armel/debian:wheezy
 cd $GitFolder
-sed -i "s/FROM debian/FROM armel\/debian/" ${FolderGlob}Dockerfile
-sed -i "/.*deb http:\/\/download.mono-project.com\/repo\/debian [0-9]\+-security main.*/d" ${FolderGlob}Dockerfile
-sed -i "s/MAINTAINER .*/MAINTAINER <https:\/\/github.com\/StefanSchoof\/aspnetarmel>/" ${FolderGlob}Dockerfile
+sed -i -e "$SedCommands" ${FolderGlob}Dockerfile
 for version in $(ls -1d $FolderGlob)
 do
 	if [[ ! " ${noBuild[@]} " =~ " ${version} " ]]; then
@@ -18,4 +21,3 @@ do
 		docker push ${ImageName}:${version%/}
 	fi
 done
-
