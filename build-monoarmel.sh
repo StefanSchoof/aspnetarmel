50ffc6206a0bbad7d0a1be41ecb6ab49f58e06bc
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2016-01-19 23:10:55 +0100

    use ext glob for noBuild; move code into function

--- build-monoarmel.sh
+++ build-monoarmel.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-noBuild=("3.8.0/" "3.10.0/")
-GitFolder="mono-docker"
-GitUrl="https://github.com/mono/docker.git"
-FolderGlob="*/"
-ImageName="aspnetarmel/armel-mono"
-SedCommands='
+shopt -s extglob
+build () {
+	local "${@}"
+	rm -rf ${GitFolder}
+	git clone ${GitUrl} ${GitFolder}
+	docker pull armel/debian:wheezy
+	cd ${GitFolder}
+	sed -i -e "$SedCommands" ${FolderGlob}Dockerfile
+	for version in $(ls -1d ${FolderGlob})
+	do
+		docker build -t ${ImageName}:${version%/} ${version}
+		docker run ${ImageName}:${version%/} /bin/bash -c "apt-get -q update && apt-get -s upgrade" | grep "^0 upgraded" || \
+			docker build --no-cache -t ${ImageName}:${version%/} ${version}
+		docker push ${ImageName}:${version%/}
+	done
+}
+
+build \
+	GitFolder="mono-docker" \
+	GitUrl="https://github.com/mono/docker.git" \
+	FolderGlob="!(3.8.0|3.10.0)/" \
+	ImageName="aspnetarmel/armel-mono" \
+	SedCommands='
 s/FROM debian/FROM armel\/debian/
 /.*deb http:\/\/download.mono-project.com\/repo\/debian [0-9]\+-security main.*/d
 s/MAINTAINER .*/MAINTAINER <https:\/\/github.com\/StefanSchoof\/aspnetarmel>/
 '
-
-rm -rf $GitFolder
-git clone $GitUrl $GitFolder
-docker pull armel/debian:wheezy
-cd $GitFolder
-sed -i -e "$SedCommands" ${FolderGlob}Dockerfile
-for version in $(ls -1d $FolderGlob)
-do
-	if [[ ! " ${noBuild[@]} " =~ " ${version} " ]]; then
-		docker build -t ${ImageName}:${version%/} $version
-		docker run ${ImageName}:${version%/} /bin/bash -c "apt-get -q update && apt-get -s upgrade" | grep "^0 upgraded" || \
-			docker build --no-cache -t ${ImageName}:${version%/} $version
-		docker push ${ImageName}:${version%/}
-	fi
-done
